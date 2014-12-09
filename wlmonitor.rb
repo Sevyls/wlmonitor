dc8557932c04bc5219154ba0177be4b2c2863235
Michael Auß <michael.auss@gmail.com>
2014-12-09 21:01:54 +0100

    Add sinatra

---
+++ wlmonitor.rb
@@ -0,0 +1,94 @@
+require 'csv'
+require 'active_support/all'
+require 'sinatra'
+
+class Haltestelle
+  attr_accessor :id, :lat, :lon, :typ, :diva, :name, :gemeinde, :gemeinde_id, :steige
+
+  # csv format: "HALTESTELLEN_ID";"TYP";"DIVA";"NAME";"GEMEINDE";"GEMEINDE_ID";"WGS84_LAT";"WGS84_LON"
+  def initialize(csv_row)
+    @id   = csv_row.field("HALTESTELLEN_ID")
+    @lat  = csv_row.field("WGS84_LAT")
+    @lon  = csv_row.field("WGS84_LON")
+    @typ  = csv_row.field("TYP")
+    @diva = csv_row.field("DIVA") # Haltestellennummer in der elektonischen Fahrplanauskunft
+    @name = csv_row.field("NAME")
+    @gemeinde = csv_row.field("GEMEINDE")
+    @gemeinde_id = csv_row.field("GEMEINDE_ID")
+    @steige = []
+  end
+end
+
+class Linie
+  # "LINIEN_ID";"BEZEICHNUNG";"REIHENFOLGE";"ECHTZEIT";"VERKEHRSMITTEL";"STAND"
+
+  attr_accessor :id, :bezeichnung, :reihenfolge, :echtzeit, :verkehrsmittel
+
+  def initialize(csv_row)
+    @id = csv_row.field("LINIEN_ID")
+    @bezeichung = csv_row.field("BEZEICHNUNG")
+    @reihenfolge = csv_row.field("REIHENFOLGE")
+    @echzeit = csv_row.field("ECHTZEIT")
+    @verkehrsmittel = csv_row.field("VERKEHRSMITTEL")
+  end
+end
+
+class Steig
+  # "STEIG_ID";"FK_LINIEN_ID";"FK_HALTESTELLEN_ID";"RICHTUNG";"REIHENFOLGE";"RBL_NUMMER";"BEREICH";"STEIG";"STEIG_WGS84_LAT";"STEIG_WGS84_LON";"STAND"
+
+  attr_accessor :id, :linie, :haltestelle, :richtung, :reihenfolge, :rbl_nr, :bereich, :steig, :lat, :lon
+
+  def initialize(csv_row)
+    @id   = csv_row.field("STEIG_ID")
+    @richtung = csv_row.field("RICHTUNG")
+    @reihenfolge = csv_row.field("REIHENFOLGE")
+    @rbl_nr = csv_row.field("RBL_NUMMER")
+    @bereich = csv_row.field("BEREICH")
+    @steig = csv_row.field("STEIG")
+    @lat  = csv_row.field("WGS84_LAT")
+    @lon  = csv_row.field("WGS84_LON")
+
+  end
+end
+
+puts "Lese Haltestellen..."
+haltestellen = Hash.new
+CSV.foreach("./wl-data/wienerlinien-ogd-haltestellen.csv", col_sep: ';', headers: true) do |row|
+  h = Haltestelle.new row
+  haltestellen[h.id] = h
+end
+puts "Fertig, insgesamt #{haltestellen.size} Haltestellen gelesen."
+
+linien = Hash.new
+
+puts "Lese Linien..."
+
+CSV.foreach("./wl-data/wienerlinien-ogd-linien.csv", col_sep: ';', headers: true) do |row|
+  l = Linie.new row
+  linien[l.id] = l
+end
+puts "Fertig, insgesamt #{linien.size} Linien gelesen."
+
+steige = Hash.new
+
+puts "Lese Steige..."
+
+CSV.foreach("./wl-data/wienerlinien-ogd-steige.csv", col_sep: ';', headers: true) do |row|
+  s = Steig.new row
+  
+  h = haltestellen[row.field("FK_HALTESTELLEN_ID")]
+  s.haltestelle = h
+
+  l = linien[row.field("FK_LINIEN_ID")]
+  s.linie = l
+
+  h.steige << s
+  steige[s.id] = s
+end
+
+puts "Fertig, insgesamt #{steige.size} Steige gelesen."
+
+
+get '/' do
+  send_file 'public/index.html'
+end
