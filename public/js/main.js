d6dad3c13a0dee58e7d8ab9a3dafa7eee1df907a
Sevyls <michael.auss@gmail.com>
2014-12-12 17:28:53 +0100

    Minor js changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-// Haltestellen Filter
 $(function() {
+	// Haltestellen Filter
 	$("#haltestelle-filter").bindWithDelay("change paste keyup", function() {
 
 		var keyword = $("#haltestelle-filter").val();
@@ -8,24 +8,26 @@ $(function() {
 		$("#haltestellen-liste > a:Contains('" + keyword + "')").show();
 	}, 200);
 
+	// Haltestelle Karte
 	if ($("#haltestelle-map").length) {
 		var koords = $('#haltestelle-koordinaten');
 		var lat = koords.find(".lat").text();
 		var lon = koords.find(".lon").text();
 		koords.find("p.koords").remove();
 
-		var latlng = new google.maps.LatLng(lat, lon);
+		var haltestellePosition = new google.maps.LatLng(lat, lon);
 
 		var mapOptions = {
-			center: latlng,
+			center: haltestellePosition,
 			zoom: 18,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		var map = new google.maps.Map($("#haltestelle-map")[0], mapOptions);
 
 		var marker = new google.maps.Marker({
-			position: latlng,
+			position: haltestellePosition,
 			map: map,
+			title: $("h1").text()
 		});
 	}
 });
